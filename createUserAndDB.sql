ca0fe09948a26b1e7f1dd619c4970d929ce3565c
Haseeb Shah <haseebs@outlook.com>
2017-01-04 17:49:49 +0500

    Reorganized the project directory structure and removed unnecessary code

--- createUserAndDB.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'test'@'localhost' IDENTIFIED BY '12345';
-CREATE DATABASE wikiDatabase;
-GRANT ALL PRIVILEGES ON wikiDatabase.* TO 'test'@'localhost';
--- docIDToTitle.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE docRefs (
-  docID INT PRIMARY KEY,
-  title varchar(250),
-  wordCount INT
-);
